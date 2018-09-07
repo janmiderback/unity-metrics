854cdefb3f5887eca5480b33395eef82ec6e3a30
Jan Miderbäck <jan.miderback@qlik.com>
2018-09-07 10:20:29 +0200

    First shot Prometheus

--- MetricsLib/Class1.cs
+++ MetricsLib/Class1.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using Prometheus;
 
 namespace MetricsLib
 {
     public class Class1
     {
+        static private MetricServer metricServer;
+
+        static Class1()
+        {
+            metricServer = new MetricServer(port: 1234);
+            metricServer.Start();
+        }
     }
 }
